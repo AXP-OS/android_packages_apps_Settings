6ac45f8d71f199f8b55e944dc900dcce3a485544
DivestOS <tavi@divested.dev>
2025-04-29 21:24:09 +0200

    uncatched DOS scripted change(s)
    Change-Id: I2056ae5961c395cd463a92ccf8c36e4c959e11c2

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -583,6 +583,12 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
                 bluetoothA2dpConfigStore, fragment));
         controllers.add(new SharedDataPreferenceController(context));
         controllers.add(new OverlaySettingsPreferenceController(context));
+        controllers.add(new SpoofSignaturePreferenceController(context));      // extendrom: signature spoofing patch
+        controllers.add(new SpoofSignatureInfo(context));       // extendrom: signature spoofing patch
+        controllers.add(new ER_CallRecPreferenceController(context)); // extendrom call recording
+        controllers.add(new ER_CallRecInfo(context)); // extendrom call recording
+        controllers.add(new ER_OrrInstallSrcPreferenceController(context)); // extendrom orr_installsrc
+        controllers.add(new ER_OrrInstallSrcInfo(context)); // extendrom orr_installsrc
 
         return controllers;
     }
