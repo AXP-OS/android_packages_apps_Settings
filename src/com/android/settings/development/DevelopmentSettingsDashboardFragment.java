95d372b553e27963ea8293028276e921f4098332
DivestOS <tavi@divested.dev>
2025-05-24 01:49:52 +0200

    uncatched DOS scripted change(s)
    Change-Id: I4008f8be5dc4050791d304152ecaadf02316b01f

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -626,6 +626,10 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         controllers.add(new StylusHandwritingPreferenceController(context));
         controllers.add(new IngressRateLimitPreferenceController((context)));
         controllers.add(new BackAnimationPreferenceController(context, fragment));
+        controllers.add(new SpoofSignaturePreferenceController(context));      // signature spoofing patch
+        controllers.add(new SpoofSignatureInfo(context));       // signature spoofing patch
+        controllers.add(new ER_CallRecPreferenceController(context)); // extendrom call recording
+        controllers.add(new ER_CallRecInfo(context)); // extendrom call recording
 
         return controllers;
     }
