414833426d7392b8534d252e7e0bed0cc3a41fbd
DivestOS <tavi@divested.dev>
2025-08-25 11:47:16 +0000

    0016-CC2_Toggle.patch
    source: https://github.com/AXP-OS/build/tree/axp/Patches/LineageOS-18.1/0016-CC2_Toggle.patch
    Change-Id: I7d27a72781b557f82671bb9c5b8f482470d114e6

--- src/com/android/settings/security/SecuritySettings.java
+++ src/com/android/settings/security/SecuritySettings.java
@@ -122,6 +122,7 @@ public class SecuritySettings extends DashboardFragment {
         securityPreferenceControllers.add(new ExecSpawnPreferenceController(context));
         securityPreferenceControllers.add(new NativeDebugPreferenceController(context));
         securityPreferenceControllers.add(new HostsPreferenceController(context));
+        securityPreferenceControllers.add(new CarrierConfig2PreferenceController(context));
         controllers.add(new PreferenceCategoryController(context, SECURITY_CATEGORY)
                 .setChildren(securityPreferenceControllers));
         controllers.addAll(securityPreferenceControllers);
