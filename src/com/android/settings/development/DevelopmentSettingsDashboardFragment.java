f927cd6ca6d1bfc85569228ee06c230c6bf3d682
AXP.OS <aosbot@axpos.org>
2025-09-15 12:03:37 +0000

    ER: intercept installation source\nF_ORR_INSTALLSRC: load controller
    Change-Id: I2da4ee5e261c1ab8292e3ca03666ff1a44e13066

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -587,6 +587,8 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         controllers.add(new SpoofSignatureInfo(context));       // extendrom: signature spoofing patch
         controllers.add(new ER_CallRecPreferenceController(context)); // extendrom call recording
         controllers.add(new ER_CallRecInfo(context)); // extendrom call recording
+        controllers.add(new ER_OrrInstallSrcPreferenceController(context)); // extendrom orr_installsrc
+        controllers.add(new ER_OrrInstallSrcInfo(context)); // extendrom orr_installsrc
 
         return controllers;
     }
