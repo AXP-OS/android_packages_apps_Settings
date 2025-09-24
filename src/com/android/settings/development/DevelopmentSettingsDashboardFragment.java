0e4fdf6bdc230d36efa117419646a9de1d22ab85
AXP.OS <aosbot@axpos.org>
2025-09-24 19:59:17 +0000

    extendrom: applied 08-packages-apps-Settings-src-com-android-settings-development-DevelopmentSettingsDashboardFragment.patch
    Change-Id: Ia6186233ceebd40296f2eebf4a41843d74d0d637

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -583,6 +583,8 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
                 bluetoothA2dpConfigStore, fragment));
         controllers.add(new SharedDataPreferenceController(context));
         controllers.add(new OverlaySettingsPreferenceController(context));
+        controllers.add(new SpoofSignaturePreferenceController(context));      // extendrom: signature spoofing patch
+        controllers.add(new SpoofSignatureInfo(context));       // extendrom: signature spoofing patch
 
         return controllers;
     }
